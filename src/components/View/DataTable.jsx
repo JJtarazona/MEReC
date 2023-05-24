cc4d18d4525130128895718645b1220a710b5d4d
Jorge <116285852+JJtarazona@users.noreply.github.com>
2023-05-23 23:08:45 -0500

    Algunos estilos

--- src/components/View/DataTable.jsx
+++ src/components/View/DataTable.jsx
@@ -67,9 +67,12 @@ function DataTable() {
       <table className="table-auto w-full">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
+            <tr
+              key={headerGroup.id}
+              className="border-b border-gray-300 text-gray-600 bg-gray-100 rounded "
+            >
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
+                <th key={header.id} className="py-5 px-4 text-left uppercase">
                   {header.isPlaceholder
                     ? null
                     : flexRender(
@@ -84,9 +87,12 @@ function DataTable() {
 
         <tbody>
           {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
+            <tr
+              key={row.id}
+              className="text-gray-600 hover:bg-slate-500  hover:text-gray-900"
+            >
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
+                <td key={cell.id} className="py-2 px-4">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
@@ -94,6 +100,13 @@ function DataTable() {
           ))}
         </tbody>
       </table>
+      <div className="mt-4 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <button className="text-gray-600 bg-gray-200 px1 rounded border border-gray-300">
+            {"<<"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
